de21aa30d55ae72a91574969ba9b5922edb29c25
Jeremy <severed.thumb.studio@gmail.com>
2024-11-06 02:26:30 -0600

    continued work on day/night mode

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ let bgColor = rootStyle.getPropertyValue('--main-bg-color');
 let fgColor = rootStyle.getPropertyValue('--main-fg-color');
 
 const modeButton = document.querySelector('.modeButton');
+const mbSlider = document.querySelector('.mbSlider');
 
 modeButton.addEventListener('click', () => {
     if (modeButton.getAttribute('nd') == 'night') {
         root.style.setProperty('--main-bg-color', fgColor);
         root.style.setProperty('--main-fg-color', bgColor);
         modeButton.setAttribute('nd', 'day');
+        mbSlider.style.left = '25px';
     } else if (modeButton.getAttribute('nd') == 'day') {
         root.style.setProperty('--main-bg-color', bgColor);
         root.style.setProperty('--main-fg-color', fgColor);
         modeButton.setAttribute('nd', 'night');
+        mbSlider.style.left = '5px';
     }
 });
